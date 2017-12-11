4b732a4ef79230ec7ca6fae6f3e91278ae6efaeb
NIRAGIRA Ferdinand <niyoferdina@yahoo.fr>
2017-12-11 19:18:21 +0200

    DataBaseMapper ajouté

---
+++ src/main/java/com/fing/acadysoft/DatabaseMapper.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fing.acadysoft;
+
+/**
+ *
+ * @author fing
+ */
+public class DatabaseMapper {
+
+    public DatabaseMapper() {
+    }
+    
+    
+}
